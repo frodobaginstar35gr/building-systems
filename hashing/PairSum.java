0a0905b9393a608cf24f2bfc6e0d047b6b857333
Alfredo Oviedo <fast.freddy.mprc@gmail.com>
2026-04-07 21:20:00 -0500

    clean up

--- hashing/PairSum.java
+++ hashing/PairSum.java
@@ -19,7 +19,5 @@ class Source {
   }
 
   public static void run() {
-    // this function behaves as `main()` for the 'run' command
-    // you may sandbox in this function , but should not remove it
   }
 }
